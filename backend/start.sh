563e0771c5b46d83c5270aaf429a68e97cf5d52d
Rakesh Reddy <rakeshreddysarikonda11@gmail.com>
2025-04-08 01:18:55 +0530

    solve errors

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-pip install -r ../model_training/requirements.txt
-npm install
-node server.js
+
+# Install ML dependencies
+pip install --upgrade pip
+pip install -r model_training/requirements.txt
+
+# Start Node server
+node backend/server.js
