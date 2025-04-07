39a15d72d4890bb2472c60da01c40b997351e91f
Rakesh Reddy <rakeshreddysarikonda11@gmail.com>
2025-04-08 01:53:10 +0530

    addd more commands start.sh to backend

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Create and activate Python virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
 # Install Python dependencies
-pip install -r model_training/requirements.txt
+pip install --upgrade pip
+pip install -r ../model_training/requirements.txt
 
-# Install Node.js dependencies
+# Install Node.js dependencies and run server
 npm install
-
-# Start the backend server
 node server.js
